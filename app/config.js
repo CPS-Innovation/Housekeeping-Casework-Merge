f1579630bcc80e58ba89d39d43f43c7a7fa01e85
Andrew Scrivener <andrew.scrivener@cps.gov.uk>
2025-10-30 15:09:44 +0000

    Project name is “Case material and communications”
    Changed following discussions within the team. Not “Housekeeping” or “Casework app” or “Case management” or “Merge”.
    
    Signed-off-by: Andrew Scrivener <andrew.scrivener@cps.gov.uk>

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Housekeeping and Casework App',
+  serviceName: 'Case material and communications',
 
   // Default port that prototype runs on
   port: '4000',
