03804385c273fd814c7499f2830ac2e14364100b
Andrew Scrivener <andrew.scrivener@cps.gov.uk>
2026-06-19 17:40:31 +0100

    Added DCF data to session
    Signed-off-by: Andrew Scrivener <andrew.scrivener@cps.gov.uk>

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,7 +1,30 @@
 const cases = require('./cases.json')
+const caseMaterials = require('./case-materials.json')
+const caseMaterialsGenerateDocuments = require('./case-materials-generate-documents.json')
+const disclosureStatuses = require('./disclosure-statuses.json')
+const disclosureNonSensitiveRows = require('./disclosure-non-sensitive-rows.json')
+const disclosureSensitiveRows = require('./disclosure-sensitive-rows.json')
+const disclosureNoLongerRelevantRows = require('./disclosure-no-longer-relevant-rows.json')
+const countsData = require('./case-indictments.json')
+const chargeLibrary = require('./charge-library.json')
 
 module.exports = {
+    caseSort: 'Name',
+    taskSort: 'Due date',
+    caseMaterials,
+    caseMaterialsGenerateDocuments,
+    disclosureStatuses,
+    disclosureNonSensitiveRows,
+    disclosureSensitiveRows,
+    disclosureNoLongerRelevantRows,
+    countsData,
+    chargeLibrary,
+
     // Legacy data used in some "Case materials" pages
     cases,
     current_version: "1.0",
-};
+
+    // ✅ NEW: rows generated when CPS marks Evidence as "unused non-sensitive"
+    disclosureAssessedUnusedRows: []
+}
+
