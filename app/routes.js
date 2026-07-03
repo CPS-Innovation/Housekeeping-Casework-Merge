a68a25bf06907abfbc088ca232b2d1ab337614eb
Andrew Scrivener <andrew.scrivener@cps.gov.uk>
2026-07-03 17:57:12 +0100

    Refactor: add v2-1 page composition
    Signed-off-by: Andrew Scrivener <andrew.scrivener@cps.gov.uk>

--- app/routes.js
+++ app/routes.js
@@ -169,13 +169,13 @@ router.get('/version-2-0/A-index/case-search', (req, res) => {
 router.get('/version-2-1/A-index', (req, res) => {
     const data = req.session.data || {}
     const caseUrnSearch = data.caseUrnSearch
-    res.render('version-2/A-index.njk', { version: '2.1', caseUrnSearch });
+    res.render('versions/v2/v2-1/A-index.njk', { version: '2.1', caseUrnSearch });
 });
 
 router.get('/version-2-1/A-index/case-search', (req, res) => {
     const data = req.session.data || {}
     const caseUrnSearch = data.caseUrnSearch
-    res.render('version-2/A-index.njk', { version: '2.1', caseUrnSearch });
+    res.render('versions/v2/v2-1/A-index.njk', { version: '2.1', caseUrnSearch });
 });
 
 router.get('/version-2-2/A-index', (req, res) => {
